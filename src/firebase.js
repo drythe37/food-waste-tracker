26452425cd1aed322c6b320e477a027175f5453f
drythe37 <111583472+drythe37@users.noreply.github.com>
2026-05-21 20:59:41 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, browserLocalPersistence, setPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { firebaseConfig } from "./config";
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
 export const db = getFirestore(app);
